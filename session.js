67b47da95bac124511305766906ab84e94231f6a
XAVIER-ui <85060236+XAVIER-ui@users.noreply.github.com>
2022-01-18 07:53:41 +0700

    Create session.js

---
+++ session.js
@@ -0,0 +1,7 @@
+{
+  "clientID": "nMkDeqvF0rPlePKrbhDcEw==",
+  "serverToken": "1@48EQa4Ik6FX53NeTX18DqSDkaQO99Jx4P2JzPk",
+  "clientToken": "LsUUcNPYwosIRpuyoTyK6VHd7XPnrPmfmLoLLQNG",
+  "encKey": "9LXbot9/NxvcwKWBeELsMttYnnRXiq9DqjSyTnTWN6M=",
+  "macKey": "4uyVhMQ99Cn3LeaTTjfyK1FBXQgXkctE57tl6T5dwqA=",
+}
